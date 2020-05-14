63dc1cc03d4f39372300cc0456da759fc5fc406d
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 21:45:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2 
     return array.join(" and ")
   elsif array.length > 2
-    array.pop
+    last_element =array.pop
     return array.join(", ").insert(-1, ", and ") 
   else 
     return array.join(", ")
