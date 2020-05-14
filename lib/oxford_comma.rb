beef8d6a06e078655276ec5d7ac200f0f2368cec
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 21:04:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.length == 2 
     return array.join(" and ")
   elsif array.length > 2
-    return array.insert(-1, " and ")
+    return array.join(" , ")
+    array.insert(-1, " and ")
   else 
     return array.join(" , ")
   end
