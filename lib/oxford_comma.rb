eaf5888c8149640e5f0771279542cfde51bcf777
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 21:36:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length > 2
     array.pop
-    return array.join(", ").push.insert(-1, ", and")
+    return array.join(", ").insert(-1, ", and")
   else 
     return array.join(", ")
   end
